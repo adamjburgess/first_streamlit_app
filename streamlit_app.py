5e71032587c531edce5daca648d0c3b9ee56903c
adamjburgess <adam.burgess@outlook.com>
2022-05-04 09:01:21 +1000

    Addedd Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Hew Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, SPinach & Rocket Smoothie')
+streamlit.text('Hard-Boild Free-Range Eggs')
